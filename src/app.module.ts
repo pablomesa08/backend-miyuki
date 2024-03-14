364d3f69348c59fc8cf02adade50c31748ccc307
Pablo Mesa <pablitomesa08@gmail.com>
2024-03-14 10:52:32 -0500

    Products and Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host:process.env.DB_HOST,
+    port:+process.env.DB_PORT,
+    database:process.env.DB_NAME,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    autoLoadEntities:true,
+    synchronize:true,
+  }), ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
