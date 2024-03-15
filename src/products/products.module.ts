73986d0e4c04ea410d2e845f200b9d67df203296
Pablo Mesa <pablitomesa08@gmail.com>
2024-03-14 21:30:10 -0500

    Products Logic

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,11 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product])]
+  imports: [TypeOrmModule.forFeature([Product]), AuthModule]
 })
 export class ProductsModule {}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>
+  ){}
+
+  private handleExceptions(error: any){
+    if(error.code === 11000){
+      throw new BadRequestException(`Product exists in DB ${JSON.stringify(error.keyvalue)}`);
+    }
+    console.log(error)
+    throw new InternalServerErrorException(`Can't create Product - Please check server logs`);
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async create(createProductDto: CreateProductDto) {
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findAll() {
+    const products = await this.productRepository.find({});
+    
+    if(!products){
+      throw new NotFoundException(`No items found`);
+    }
+
+    return products;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({id:id});
+    
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({id:id,
+      ...updateProductDto});
+
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    await this.productRepository.save(product);
+    return product;
+  }
+
+  async remove(id: string) {
+    const product = await this.productRepository.findOneBy({id:id});
+    
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    await this.productRepository.remove(product);
+    
   }
 }
