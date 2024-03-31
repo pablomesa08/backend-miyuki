3902522643217ad60ac055958471cd3e2f220974
Pablo Mesa <pablitomesa08@gmail.com>
2024-03-31 13:12:56 -0500

    Cart Logic

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cart } from './entities/cart.entity';
 
 @Injectable()
 export class CartService {
-  create(createCartDto: CreateCartDto) {
-    return 'This action adds a new cart';
+  constructor(
+    @InjectRepository(Cart)
+    private cartRepository: Repository<Cart>
+  ){}
+
+  private handleExceptions(error: any){
+    if(error.code === 11000){
+      throw new BadRequestException(`Cart exists in DB ${JSON.stringify(error.keyvalue)}`);
+    }
+    console.log(error)
+    throw new InternalServerErrorException(`Can't create Cart - Please check server logs`);
   }
 
-  findAll() {
-    return `This action returns all cart`;
+  async create(createCartDto: CreateCartDto) {
+    try {
+
+      const cart= this.cartRepository.create(createCartDto);
+      await this.cartRepository.save(cart);
+      return cart;
+
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} cart`;
+  async findAll() {
+    const carts = await this.cartRepository.find({});
+    
+    if(!carts){
+      throw new NotFoundException(`No items found`);
+    }
+
+    return carts;
   }
 
-  update(id: string, updateCartDto: UpdateCartDto) {
-    return `This action updates a #${id} cart`;
+  async findOne(id: string) {
+    const cart= await this.cartRepository.findOneBy({id:id});
+    
+    if(!cart){
+      throw new NotFoundException(`Cart with id ${id} not found`);
+    }
+
+    return cart;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} cart`;
+  async update(id: string, updateCartDto: UpdateCartDto) {
+    const cart = await this.cartRepository.preload({id:id,
+      ...updateCartDto});
+
+    if(!cart){
+      throw new NotFoundException(`Cart with id ${id} not found`);
+    }
+
+    await this.cartRepository.save(cart);
+    return cart;
+  }
+
+  async remove(id: string) {
+    const cart = await this.cartRepository.findOneBy({id:id});
+    
+    if(!cart){
+      throw new NotFoundException(`Cart with id ${id} not found`);
+    }
+    await this.cartRepository.remove(cart);
   }
 }
