b9ef24381adef40c1e5926bbf44c2ee95f5ff91a
Pablo Mesa <pablitomesa08@gmail.com>
2024-05-06 08:44:09 -0500

    Promo Module

--- src/promotion/promotion.module.ts
+++ src/promotion/promotion.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PromotionService } from './promotion.service';
 import { PromotionController } from './promotion.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { Promotion } from './entities/promotion.entity';
 
 @Module({
   controllers: [PromotionController],
   providers: [PromotionService],
+  imports: [TypeOrmModule.forFeature([Promotion]), AuthModule],
+  exports: [TypeOrmModule, PromotionService]
 })
 export class PromotionModule {}
